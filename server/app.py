d0ebeb725218165468c733d98a66704924d99f29
Andrew Sigei <andrew.sigei@student.moringaschool.com>
2026-01-19 01:06:32 +0300

    final commit

--- server/app.py
+++ server/app.py
@@ -1,10 +1,6 @@
-#!/usr/bin/env python3
-
-from flask import Flask, make_response, jsonify, request, session
-from flask_migrate import Migrate
-from flask_restful import Api, Resource
-
-from models import db, Article, User
+from flask import Flask, request, session, jsonify
+from flask_restful import Resource, Api
+from models import db, User
 
 app = Flask(__name__)
 app.secret_key = b'Y\xf1Xz\x00\xad|eQ\x80t \xca\x1a\x10K'
@@ -12,46 +8,51 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.json.compact = False
 
-migrate = Migrate(app, db)
-
 db.init_app(app)
-
 api = Api(app)
 
-class ClearSession(Resource):
-
-    def delete(self):
+class Login(Resource):
     
-        session['page_views'] = None
+    def post(self):
+        # Get username from request JSON
+        username = request.get_json()['username']
+        
+        # Find user by username
+        user = User.query.filter(User.username == username).first()
+        
+        # Set user_id in session
+        session['user_id'] = user.id
+        
+        # Return user as JSON with 200 status
+        return user.to_dict(), 200
+
+class Logout(Resource):
+    
+    def delete(self):
+        # Remove user_id from session
         session['user_id'] = None
+        
+        # Return no data with 204 status
+        return '', 204
 
-        return {}, 204
-
-class IndexArticle(Resource):
+class CheckSession(Resource):
     
     def get(self):
-        articles = [article.to_dict() for article in Article.query.all()]
-        return articles, 200
+        # Get user_id from session
+        user_id = session.get('user_id')
+        
+        # If user_id exists in session, find and return user
+        if user_id:
+            user = User.query.filter(User.id == user_id).first()
+            return user.to_dict(), 200
+        
+        # If no user_id in session, return 401
+        return {}, 401
 
-class ShowArticle(Resource):
-
-    def get(self, id):
-        session['page_views'] = 0 if not session.get('page_views') else session.get('page_views')
-        session['page_views'] += 1
-
-        if session['page_views'] <= 3:
-
-            article = Article.query.filter(Article.id == id).first()
-            article_json = jsonify(article.to_dict())
-
-            return make_response(article_json, 200)
-
-        return {'message': 'Maximum pageview limit reached'}, 401
-
-api.add_resource(ClearSession, '/clear')
-api.add_resource(IndexArticle, '/articles')
-api.add_resource(ShowArticle, '/articles/<int:id>')
-
+# Register resources with API
+api.add_resource(Login, '/login')
+api.add_resource(Logout, '/logout')
+api.add_resource(CheckSession, '/check_session')
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
